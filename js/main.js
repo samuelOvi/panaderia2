5cfe1fc8a5178b88070ecce531587e7ef2dda418
samuelOvi <119080554+samuelOvi@users.noreply.github.com>
2023-01-23 22:04:59 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+function ancho(){
+  var items=5
+  if(screen.width< 800){
+      items =3;
+  }
+  return items
+}
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: ancho(),
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
